21c752427ec417ab0729cfd977d69d07bd86f68d
Chayan Pandit <chayanpandit07@gmail.com>
2024-07-02 23:18:34 +0530

    frontend separate

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,5 +1,5 @@
 import { SignupInput } from "@chayanpandit/medium-common"
-import { ChangeEvent, ChangeEventHandler, useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import axios from "axios"
 import { BACKEND_URL } from "../config"
